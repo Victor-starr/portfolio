08c2f9202d9a8b43eadea7a6c5a36b4d5d07eb39
Victor-starr <v94625230@gmail.com>
2025-08-30 21:49:10 +0300

    Add Photo-Starr project details and short descriptions to Project component

--- src/Project.jsx
+++ src/Project.jsx
@@ -6,6 +6,8 @@ import { useState } from "react";
 const project = {
   tastylist: {
     title: "Tasty-List",
+    short_description:
+      "A web application that allows users to create, manage, and share their favorite recipes.",
     description:
       "Tasty-List is a web app for organizing, managing, and sharing your favorite recipes. Create collections, add instructions and images, and share with friends or the public. Perfect for meal planning and discovering new dishes.",
     image: "./tastylistBanner.png",
@@ -14,6 +16,8 @@ const project = {
   },
   deezerStarr: {
     title: "Deezer-Starr",
+    short_description:
+      "A music streaming application that allows users to search and play their favorite songs.",
     description:
       "Deezer-Starr is a music streaming app to search, discover, and play songs from a vast library. Curate playlists, explore trending tracks, and enjoy a sleek, responsive design. Built for fast search and a great listening experience.",
     image: "./deezerstarrBanner.png",
@@ -22,11 +26,23 @@ const project = {
   },
   tableStarr: {
     title: "Table-Starr",
+    short_description:
+      "A web application designed to help users keep track of their orders and split the bill with friends.",
     description:
       "Table-Starr helps you and your friends keep track of orders and split the bill easily—perfect for going out to eat or sharing expenses.",
     demo: "https://table-starr.vercel.app/",
     github: "https://github.com/Victor-starr/Table-Starr",
     image: "./tableStarrBanner.png",
+  },
+  photoStarr: {
+    title: "Photo-Starr",
+    short_description:
+      "A fun website for taking, customizing, and sharing photos in themed sessions.",
+    description:
+      "Photo Booth is a fun website for taking, customizing, and sharing photos in themed sessions. Great for parties, events, or just for fun with friends.",
+    image: "./photo-starr-banner.png",
+    demo: "https://photo-starr.vercel.app",
+    github: "https://github.com/Victor-starr/photo-booth",
   },
 };
 
